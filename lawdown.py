0836f209258d3d19405d2ac5923159d7fbd3a38c
Joachim Mueller <joacmue@gmail.com>
2021-04-03 18:19:10 +0200

    Some suggested changes from the PR

--- lawdown.py
+++ lawdown.py
@@ -512,6 +512,7 @@ class LawToMarkdown(sax.ContentHandler):
         abk = re.sub(r'[^\w-]', '_', abk)
         self.filename = abk
 
+
 def law_to_markdown(filein, fileout=None, name=None):
     ret = False
     if fileout is None:
